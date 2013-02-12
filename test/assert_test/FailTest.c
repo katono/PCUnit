dbebca146af33cb9a8c861c06bdfbd8e926d24d9
katono <katono123@gmail.com>
2013-02-12 22:39:33 +0900

    test_message*を追加。

--- test/assert_test/FailTest.c
+++ test/assert_test/FailTest.c
@@ -23,20 +23,37 @@ static int teardown(void)
 
 static void test_fail1(void)
 {
-	PCU_MESSAGE("test_message");
 	PCU_FAIL("test_fail");
 }
 
 static void test_fail2(void)
 {
-	PCU_MESSAGE(PCU_format("test_message: %d,%#x, %c, %04d, %s, 0x%08X, %+d, %-8x,% d, %10d", 1, 2, '3', 4, "5", 6, 7, 8, -9, 10));
 	PCU_FAIL(PCU_format("test_fail: %d,%#x, %c, %04d, %s, 0x%08X, %+d, %-8x,% d, %10d", 1, 2, '3', 4, "5", 6, 7, 8, -9, 10));
+}
+
+static void test_message1(void)
+{
+	PCU_MESSAGE("test_message");
+}
+
+static void test_message2(void)
+{
+	PCU_MESSAGE(PCU_format("test_message: %d,%#x, %c, %04d, %s, 0x%08X, %+d, %-8x,% d, %10d", 1, 2, '3', 4, "5", 6, 7, 8, -9, 10));
+}
+
+static void test_message_fail(void)
+{
+	PCU_MESSAGE("test_message");
+	PCU_FAIL("test_fail");
 }
 
 
 static PCU_Test tests[] = {
 	PCU_TEST(test_fail1),
 	PCU_TEST(test_fail2),
+	PCU_TEST(test_message1),
+	PCU_TEST(test_message2),
+	PCU_TEST(test_message_fail),
 };
 
 PCU_Suite *FailTest_suite(void)
